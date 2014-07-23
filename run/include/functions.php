219e91dd946bf31a3b0430036f9bda3acb227304
Cade Scroggins <scroggins.cade@gmail.com>
2014-07-23 11:22:32 -0700

    add command_exist function in php to check for shell commands

--- run/include/functions.php
+++ run/include/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function command_exist($cmd) {
+  $returnVal = shell_exec("which $cmd");
+  return (empty($returnVal) ? false : true);
+}
+
 function bail_out($code, $message) {
 	exit(json_encode(array('error' => $code, 'message' => $message)));
 }
