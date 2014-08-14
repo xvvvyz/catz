3f8c871c5f0d7be319ed60b0a2ab8fbb8fdb5d96
Cade Scroggins <scroggins.cade@gmail.com>
2014-08-14 12:21:20 -0700

    whitespace and update variable name

--- run/fetch/songza.php
+++ run/fetch/songza.php
@@ -2,8 +2,8 @@
 
 include '../include/functions.php';
 
-$url = (isset($_POST["url"]) ? $_POST["url"] : "http://songza.com/listen/today-s-trap-and-moombahton-rump-shakers-songza/");
-$stationId = (isset($_POST["stationId"]) ? $_POST["stationId"] : "");
+$url = (isset($_POST["url"]) ? $_POST["url"] : "http://songza.com/listen/electronic-all-nighter-SamsungMobileUSA/");
+$stationId = (isset($_POST["stationId"]) ? $_POST["stationId"] : "1718994");
 $sessionId = (isset($_POST["sessionId"]) ? $_POST["sessionId"] : "IPAmNpAX02YTL8CPgc");
 $userAgent = (isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:26.0) Gecko/20100101 Firefox/26.0");
 
@@ -34,12 +34,12 @@ if (empty($stationId)) {
 
 	// throw it into an array to add to output json
 	$stationArray = array("station_id" => $stationId,
-						  "station_slug" => $stationSlug,
-						  "station_name" => $stationName,
-						  "station_100" => "http://songza.com/api/1/station/".$stationId."/image?size=133",
-						  "station_500" => "http://songza.com/api/1/station/".$stationId."/image?size=500&name=image.jpg",
-						  "creator_name" => $creatorName,
-						  "total_tracks" => $songCount);
+		"station_slug" => $stationSlug,
+		"station_name" => $stationName,
+		"station_100" => "http://songza.com/api/1/station/".$stationId."/image?size=133",
+		"station_500" => "http://songza.com/api/1/station/".$stationId."/image?size=500&name=image.jpg",
+		"creator_name" => $creatorName,
+		"total_tracks" => $songCount);
 }
 
 
@@ -53,12 +53,12 @@ curl_exec($ch);
 $songArray = json_decode(curl_exec($ch), true);
 curl_close($ch);
 
-$add = array("error" => 0);
+$error = array("error" => 0);
 
 if (isset($stationArray)) {
-	$output = json_encode(array_merge($stationArray, $songArray, $add));
+	$output = json_encode(array_merge($stationArray, $songArray, $error));
 } else {
-	$output = json_encode(array_merge($songArray, $add));
+	$output = json_encode(array_merge($songArray, $error));
 }
 
 echo $output;
