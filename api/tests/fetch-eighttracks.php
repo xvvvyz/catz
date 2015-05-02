58fe3deaabb0637204c2434eda22afafc7ecb70d
Cade Scroggins <scroggins.cade@gmail.com>
2015-05-02 11:31:49 -0700

    add tests dir

---
+++ api/tests/fetch-eighttracks.php
@@ -0,0 +1,11 @@
+<?php
+
+$debugging = true;
+
+$_POST = array(
+  "what" => "fetch",
+  "url" => "http://8tracks.com/musicbeats/programming",
+  "datatype" => "array"
+);
+
+require "do.php";
---
+++ api/tests/fetch-songza.php
@@ -0,0 +1,13 @@
+<?php
+
+$debugging = true;
+
+$_POST = array(
+  "what" => "fetch",
+  "url" => "http://songza.com/listen/downtempo-instrumentals-tenmillionsounds/",
+  "session_id" => "69cao5mDwek42Zqzhn",
+  "station_id" => "1399111",
+  "datatype" => "array"
+);
+
+require "do.php";
