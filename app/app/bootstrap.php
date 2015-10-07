f5e8d5849880b8ffcedf767aecf90822de698005
Malachi Soord <inverse.chi@gmail.com>
2015-10-07 22:28:46 +0200

    Refactored into App

--- app/app/bootstrap.php
+++ app/app/bootstrap.php
@@ -1,131 +1,14 @@
 <?php
 
-use Omgcatz\Includes\Archive;
-use Omgcatz\Includes\Curl;
-use Omgcatz\Includes\Database;
-use Omgcatz\Includes\Delegate;
-use Omgcatz\Includes\Download;
-use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Knp\Provider\ConsoleServiceProvider;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new josegonzalez\Dotenv\Loader(__DIR__ . '/../.env');
 $loader->parse();
-$loader->putenv(true);
+$config = $loader->toArray();
 
-date_default_timezone_set(getenv('TIMEZONE') ?: 'Europe/Berlin');
+date_default_timezone_set($config['TIMEZONE'] ?: 'Europe/Berlin');
 
-$app = new Silex\Application();
+$config = array_merge($config, ['app_dir' => __DIR__]);
 
-$app['debug'] = getenv('DEBUG') ?: false;
-$app['env'] = $app['debug'] ? 'dev' : 'prod';
-
-$app['app_dir'] = __DIR__;
-$app['app_download_dir'] = __DIR__ . '/download';
-
-if ($app['debug']) {
-  ini_set("display_errors", "On");
-  error_reporting(E_ALL | E_STRICT);
-} else {
-  ini_set("display_errors", "Off");
-  error_reporting(0);
-}
-
-/**
- * Register database
- */
-$app['database'] = $app->share(function () {
-  return new Database(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
-});
-
-/**
- * Register curl
- */
-$app['curl'] = function () {
-  return new Curl();
-};
-
-/**
- * Register Delegate
- */
-$app['delegate'] = function () use ($app) {
-  return new Delegate($app['database']);
-};
-
-/**
- * Register Download
- */
-$app['download'] = $app->share(function () use ($app) {
-  return new Download($app['app_dir']);
-});
-
-$app['archive'] = $app->share(function () use ($app) {
-  return new Archive($app['app_dir']);
-});
-
-/**
- * Command line land
- */
-$app->register(new ConsoleServiceProvider(), array(
-  'console.name' => 'OmgCatz',
-  'console.version' => '1.0.0',
-  'console.project_directory' => __DIR__ . '/..'
-));
-
-/**
- * Register Twig
- */
-$app->register(new Silex\Provider\TwigServiceProvider(), [
-  'twig.path' => __DIR__ . '/views',
-  'twig.options' => [
-    'debug' => $app['debug'],
-    'cache' => __DIR__ . sprintf("/cache/%s/twig", $app['env'])
-  ]
-]);
-
-/**
- * Add URL generation
- */
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
-/**
- * Add asset pipeline
- */
-$app['twig'] = $app->share($app->extend('twig', function (Twig_Environment $twig) {
-  $twig->addFunction(new Twig_SimpleFunction('asset', function ($asset) {
-    return sprintf('/%s', ltrim($asset, '/'));
-  }));
-
-  return $twig;
-}));
-
-/**
- * Register logging
- */
-$app->register(new Silex\Provider\MonologServiceProvider(), [
-  'monolog.logfile' => __DIR__ . sprintf("/logs/%s.log", $app['env']),
-]);
-
-/**
- * Register error handler
- */
-$app->error(function (\Exception $e, $code) {
-  $code = 500;
-  if ($e instanceof FileNotFoundException) {
-    $code = 404;
-  }
-  return new JsonResponse(['error' => $e->getMessage()], $code);
-});
-
-/**
- * Routing land
- */
-$app->get('/', '\Omgcatz\Controller\SiteController::indexAction')->bind('home');
-$app->post('/archive', '\Omgcatz\Controller\SiteController::archiveAction')->bind('archive');
-$app->post('/download', '\Omgcatz\Controller\SiteController::downloadAction')->bind('download');
-$app->post('/fetch', '\Omgcatz\Controller\SiteController::fetchAction')->bind('fetch');
-$app->get('/magic', '\Omgcatz\Controller\SiteController::magicAction')->bind('magic');
-
+$app = new Omgcatz\App($config);
 return $app;
---
+++ app/src/App.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Omgcatz;
+
+use Knp\Provider\ConsoleServiceProvider;
+use Omgcatz\Includes\Archive;
+use Omgcatz\Includes\Curl;
+use Omgcatz\Includes\Database;
+use Omgcatz\Includes\Delegate;
+use Omgcatz\Includes\Download;
+use Silex\Application;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Twig_Environment;
+use Twig_SimpleFunction;
+
+class App extends Application
+{
+  /**
+   * @param array $config
+   */
+  public function __construct(array $config = []) {
+    parent::__construct();
+
+    $this['debug'] = $config['DEBUG'] ?: false;
+    $this['env'] = $this['debug'] ? 'dev' : 'prod';
+
+    $this['app_dir'] = $config['app_dir'];
+    $this['app_download_dir'] = $this['app_dir'] . '/download';
+
+    if ($this['debug']) {
+      ini_set("display_errors", "On");
+      error_reporting(E_ALL | E_STRICT);
+    } else {
+      ini_set("display_errors", "Off");
+      error_reporting(0);
+    }
+
+    /**
+     * Register database
+     */
+    $this['database'] = $this->share(function () use ($config) {
+      return new Database($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']);
+    });
+
+    /**
+     * Register curl
+     */
+    $this['curl'] = function () {
+      return new Curl();
+    };
+
+    /**
+     * Register Delegate
+     */
+    $this['delegate'] = function () {
+      return new Delegate($this['database']);
+    };
+
+    /**
+     * Register Download
+     */
+    $this['download'] = $this->share(function ()  {
+      return new Download($this['app_dir']);
+    });
+
+    $this['archive'] = $this->share(function () {
+      return new Archive($this['app_dir']);
+    });
+
+    /**
+     * Command line land
+     */
+    $this->register(new ConsoleServiceProvider(), [
+      'console.name' => 'OmgCatz',
+      'console.version' => '1.0.0',
+      'console.project_directory' => $this['app_dir'] . '/..'
+    ]);
+
+    /**
+     * Register Twig
+     */
+    $this->register(new TwigServiceProvider(), [
+      'twig.path' =>  $this['app_dir'] . '/views',
+      'twig.options' => [
+        'debug' => $this['debug'],
+        'cache' =>  $this['app_dir'] . sprintf("/cache/%s/twig", $this['env'])
+      ]
+    ]);
+
+    /**
+     * Add URL generation
+     */
+    $this->register(new UrlGeneratorServiceProvider());
+
+    /**
+     * Add asset pipeline
+     */
+    $this['twig'] = $this->share($this->extend('twig', function (Twig_Environment $twig) {
+      $twig->addFunction(new Twig_SimpleFunction('asset', function ($asset) {
+        return sprintf('/%s', ltrim($asset, '/'));
+      }));
+
+      return $twig;
+    }));
+
+    /**
+     * Register logging
+     */
+    $this->register(new MonologServiceProvider(), [
+      'monolog.logfile' => $this['app_dir'] . sprintf("/logs/%s.log", $this['env']),
+    ]);
+
+    /**
+     * Register error handler
+     */
+    $this->error(function (\Exception $e, $code) {
+      $code = 500;
+      if ($e instanceof FileNotFoundException) {
+        $code = 404;
+      }
+      return new JsonResponse(['error' => $e->getMessage()], $code);
+    });
+
+    /**
+     * Routing land
+     */
+    $this->get('/', '\Omgcatz\Controller\SiteController::indexAction')->bind('home');
+    $this->post('/archive', '\Omgcatz\Controller\SiteController::archiveAction')->bind('archive');
+    $this->post('/download', '\Omgcatz\Controller\SiteController::downloadAction')->bind('download');
+    $this->post('/fetch', '\Omgcatz\Controller\SiteController::fetchAction')->bind('fetch');
+    $this->get('/magic', '\Omgcatz\Controller\SiteController::magicAction')->bind('magic');
+  }
+}
