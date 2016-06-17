ce39e3949b9f3e144ce302df6f7832fee369f580
Cade Scroggins <git@cadejs.com>
2016-06-16 19:16:11 -0700

    add scss linter

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,11 @@
 const base = require('./webpack.config.base.js');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
+const sassLintPlugin = require('sasslint-webpack-plugin');
 
 module.exports = base.merge({
   devtool: 'eval',
   plugins: [
-    new LiveReloadPlugin({appendScriptTag: true})
+    new LiveReloadPlugin({appendScriptTag: true}),
+    new sassLintPlugin({glob: 'app/scss/*scss'})
   ]
 });
