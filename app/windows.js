bf3044d11eb42a0cb92536067cb1a17738b5e597
Cade Scroggins <git@cadejs.com>
2016-08-10 18:53:28 -0700

    refactor windows.js

--- app/windows.js
+++ app/windows.js
@@ -1,22 +1,19 @@
 import {BrowserWindow} from 'electron';
 
-function createMainWindow() {
-  let win = windows.main = new BrowserWindow({
-    title: 'Catz',
-    width: 500,
-    minWidth: 500,
-    height: 600,
-    minHeight: 300,
-    titleBarStyle: 'hidden-inset',
-    backgroundColor: '#fff',
-    darkTheme: true,
-    useContentSize: true
-  });
+const windows = module.exports = {
+  createMainWindow: () => {
+    let win = windows.main = new BrowserWindow({
+      title: 'Catz',
+      width: 500,
+      minWidth: 500,
+      height: 600,
+      minHeight: 300,
+      titleBarStyle: 'hidden-inset',
+      backgroundColor: '#fff',
+      useContentSize: true
+    });
 
-  win.loadURL(`file://${__dirname}/index.html`);
-  win.on('closed', () => windows.main = null);
-}
-
-let windows = module.exports = {
-  createMainWindow
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.on('closed', () => windows.main = null);
+  }
 };
